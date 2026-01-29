5aff6d2386bba298b63b411c74317c56ae7e0f28
menjivarj <jonathan.menjivar@bruins.belmont.edu>
2026-01-29 13:08:21 -0800

    Player Sweet Holding and Instantiation
    Created sweet for player to hold and move and creates new sweet instantiations for the player to drop

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,7 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+
     public float speed;
+    public GameObject sweet;
+    private GameObject sweetHeld;
+    public float offY;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         
@@ -11,7 +16,27 @@ public class PlayerBehaviour : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        Keyboard k = Keyboard.current;
+
+        if (sweetHeld != null){
+            Vector3 playerPos = transform.position;
+            Vector3 sweetOffset = new Vector3(0.0f, offY, 0.0f);
+            sweetHeld.transform.position = playerPos + sweetOffset;
+        } else {
+            sweetHeld = Instantiate(sweet, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        }
+
+            Keyboard k = Keyboard.current;
+
+        if (k.spaceKey.wasPressedThisFrame){
+            Rigidbody2D body = sweetHeld.GetComponent<Rigidbody2D>();
+            body.gravityScale = 1.0f;
+
+            Collider2D collider = sweetHeld.GetComponent<Collider2D>();
+            collider.enabled = true;
+
+            sweetHeld = null;
+        }
+
         if (k.aKey.isPressed || k.leftArrowKey.isPressed) {
             Vector3 newPos = transform.position;
             newPos.x = newPos.x - speed;
