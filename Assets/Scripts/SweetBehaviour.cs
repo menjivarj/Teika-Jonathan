7acb1e2858db0d110d1c4b481e082ce91cad2a1a
menjivarj <jonathan.menjivar@bruins.belmont.edu>
2026-02-12 13:16:36 -0800

    Game Over Screen
    Added game over screen to be displayed when the sweets run out the timer colliding on the top border

--- Assets/Scripts/SweetBehaviour.cs
+++ Assets/Scripts/SweetBehaviour.cs
@@ -8,29 +8,35 @@ public class SweetBehaviour : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("TopBorder")) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("TopBorder"))
+        {
             timeStart = Time.time;
         }
     }
 
-    private void OnTriggerStay2D(Collider2D other) {
-        if (other.gameObject.CompareTag("TopBorder") && (Time.time - timeStart) > timeout) {
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("TopBorder") && (Time.time - timeStart) > timeout)
+        {
             print("Game Over");
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.CompareTag("TopBorder")) {
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("TopBorder"))
+        {
             timeStart = 0.0f;
         }
     }
